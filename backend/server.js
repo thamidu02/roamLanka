bd83fa123503361dff8c829244aeb107f8ea6767
IT24104083 <137370525+thamidu02@users.noreply.github.com>
2026-09-04 11:01:17 +0530

    added middleware and resolved conflicts having with server.js

--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,7 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 import placeRoutes from "./routes/placeRoutes.js";
-
-const authRoutes = require("./routes/authRoutes");
+import authRoutes from "./routes/authRoutes.js";
 
 dotenv.config();
 
